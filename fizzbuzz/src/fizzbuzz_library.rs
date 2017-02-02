703e5818ae1fbb2a36ce861862ed96e15b72b416
somkiat <somkiat.p@gmail.com>
2017-02-03 01:08:29 +0700

    Solving with matching

--- fizzbuzz/src/fizzbuzz_library.rs
+++ fizzbuzz/src/fizzbuzz_library.rs
@@ -1,10 +1,11 @@
 pub fn say(number: i8) -> String {
-    if number%15 == 0 {
-        return "fizzbuzz".to_string()
-    } else if number%3 == 0 {
-        return "fizz".to_string()
-    } else if number%5 == 0 {
-        return "buzz".to_string()
-    }
-    number.to_string()
+    let is_fizz = number%3 == 0;
+    let is_buzz = number%5 == 0;
+    let result = match (is_fizz, is_buzz) {
+                    (true, true) => "fizzbuzz".to_string(),
+                    (true, false) => "fizz".to_string(),
+                    (false, true) => "buzz".to_string(),
+                    (false, false) => number.to_string()
+                };
+    return result;
 }
