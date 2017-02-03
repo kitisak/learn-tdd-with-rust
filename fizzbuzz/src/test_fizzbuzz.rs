568db64def773dffb5649399fb76c5efc578e6f5
somkiat <somkiat.p@gmail.com>
2017-02-03 08:20:35 +0700

    Use mod

--- fizzbuzz/src/test_fizzbuzz.rs
+++ fizzbuzz/src/test_fizzbuzz.rs
@@ -1,23 +1,25 @@
+use fizzbuzz_library::say;
+
 #[test]
 fn say_number() {
-    assert_eq!(::fizzbuzz_library::say(1), "1");
-    assert_eq!(::fizzbuzz_library::say(2), "2")
+    assert_eq!(say(1), "1");
+    assert_eq!(say(2), "2")
 }
 
 #[test]
 fn say_fizz() {
-    assert_eq!(::fizzbuzz_library::say(3), "fizz");
-    assert_eq!(::fizzbuzz_library::say(6), "fizz")
+    assert_eq!(say(3), "fizz");
+    assert_eq!(say(6), "fizz")
 }
 
 #[test]
 fn say_buzz() {
-    assert_eq!(::fizzbuzz_library::say(5), "buzz");
-    assert_eq!(::fizzbuzz_library::say(10), "buzz")
+    assert_eq!(say(5), "buzz");
+    assert_eq!(say(10), "buzz")
 }
 
 #[test]
 fn say_fizzbuzz() {
-    assert_eq!(::fizzbuzz_library::say(15), "fizzbuzz");
-    assert_eq!(::fizzbuzz_library::say(30), "fizzbuzz")
+    assert_eq!(say(15), "fizzbuzz");
+    assert_eq!(say(30), "fizzbuzz")
 }
